091dea6601560777a65a6837e6d15c0a460e3d4d
zainabtura <zainabtura2002@gmail.com>
2024-11-06 21:13:29 +0300

    added dropdown menu compnent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
 
-import Hero from './components/Hero/Hero';
-import Footer from './components/Footer/Footer';
-import Logo from './components/Logo/Logo';
-import EventsSection from './components/EventsSection/EventsSection';
-import ContactSection from './components/ContactSection/ContactSection';
-import TrustedCompanies from './components/TrustedCompanies/TrustedCompanies';
-import OrganizationCTA from './components/OrganizationCTA/OrganizationCTA';
-import Navigation from './components/Navigation/Navigation';
-import ChancelyHomepageNotLoggedIn from './components/ChancelyHomepageNotLoggedIn/ChancelyHomepageNotLoggedIn';
+import DropdownMenu from './components/DropdownMenu/DropdownMenu';
+
 
 function App() {
   return (
     <div>
-      <OpportunityHomePage/>
-        
+      <DropdownMenu/>
     </div>
         
   );
