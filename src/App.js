3db35545137d3ab4b4f9670801930f56c7f2c4e2
zainabtura <zainabtura2002@gmail.com>
2024-11-08 19:30:36 +0100

    orgnizer header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import AboutUs from './components/AboutUsPage/AboutUs';
+import OrgnizerHeader from './components/OrgnizerHeader/OrgnizerHeader';
 
-
-
-import ChancelyHomepageNotLoggedIn from './components/ChancelyHomepageNotLoggedIn/ChancelyHomepageNotLoggedIn';
-import CompanyBox from './components/CompanyBox/CompanyBox';
-import OpportunityHomePage from './components/OpportunityHomePage/OpportunityHomePage';
-import BrowseOpportunities from './components/User Pages/BrowseOpportunities/BrowseOpportunities';
 
 function App() {
   return (
     <div>
-     <AboutUs/>
+     <OrgnizerHeader/>
         
     </div>
         
