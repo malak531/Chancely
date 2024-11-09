6de285d41f1e9c0befb5794b7d7c16a0bc091d28
zainabtura <zainabtura2002@gmail.com>
2024-11-09 22:07:23 +0100

    org registration page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import AboutUs from './components/AboutUsPage/AboutUs';
-import ChancelyHomepageNotLoggedIn from './components/ChancelyHomepageNotLoggedIn/ChancelyHomepageNotLoggedIn';
-import CompanyBox from './components/CompanyBox/CompanyBox';
-import OpportunityHomePage from './components/OpportunityHomePage/OpportunityHomePage';
-import BrowseOpportunities from './components/User Pages/BrowseOpportunities/BrowseOpportunities';
-import OrganizationProfile from './components/OrganizationProfilePage/OrganizationProfile/OrganizationProfile';
-import EventCreation from './components/OrganizationPages/EventCreation/EventCreation';
-import OrgnizerHeader from './components/OrgnizerHeader/OrgnizerHeader';
-import EditUserProfile from './components/User Pages/Profile/EditUserProfile';
-import EditEventOrganizer from './components/EditEventOrganizer/EditEventOrganizer';
-import OpportunityInfoTop from './components/OpportunityInfo/OpportunityInfoTop';
-import OpportunityInfo from './components/OpportunityInfo/OpportunityInfo';
-import OpportunityDescription from './components/OpportunityInfo/OpportunityDescription';
-import LoginForm from './components/LoginPage/LoginForm';
-import LoginPage from './components/LoginPage/LoginPage';
-import StepOne from './components/Sign Up Pages User/SelectType/StepOne';
-import StepTwo from './components/Sign Up Pages User/StepTwo/StepTwo';
-import InterestSelection from './components/Sign Up Pages User/StepThree/InterestSelection';
-import AccountCreationSuccess from './components/Sign Up Pages User/StepFour/AccountCreationSuccess';
+
+import EventOrganizerRegistration from './components/EventOrganizerRegistration/EventOrganizerRegistration';
+
 function App() {
   return (
     <div >
-      <AccountCreationSuccess/>
+      <EventOrganizerRegistration/>
     </div>
         
   );
